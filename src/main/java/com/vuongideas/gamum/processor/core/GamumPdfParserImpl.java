d058d7c41837e12fb3b08f3079c81634d828e6d2
Bao Vuong <vqbao@lv5.org>
2019-12-22 15:20:56 -0500

    able to read Tj, Tf, and Do operations

--- src/main/java/com/vuongideas/gamum/processor/core/GamumPdfParserImpl.java
+++ src/main/java/com/vuongideas/gamum/processor/core/GamumPdfParserImpl.java
@@ -1,29 +1,12 @@
 package com.vuongideas.gamum.processor.core;
 
-import com.vuongideas.gamum.processor.core.textdecoder.UnicodeTextDecoder;
 import com.vuongideas.gamum.processor.model.ExtractedDocument;
-import com.vuongideas.gamum.processor.model.PdfData;
-import com.vuongideas.gamum.processor.model.fragment.ExtractedFragment;
-import com.vuongideas.gamum.processor.model.fragment.ExtractedTextFragment;
-import org.apache.fontbox.cmap.CMap;
-import org.apache.pdfbox.contentstream.PDContentStream;
-import org.apache.pdfbox.cos.*;
-import org.apache.pdfbox.pdfparser.PDFStreamParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDResources;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 
 @Component
@@ -35,36 +18,14 @@ public class GamumPdfParserImpl implements GamumPdfParser {
 
         PageExtractor extractor = new PageExtractor();
 
-        for (PDPage page : doc.getPages()) {
-
-            extractor.processPage(page);
-
-//            PDFStreamParser parser = new PDFStreamParser(page);
-//            parser.parse();
-//            List<Object> tokens = parser.getTokens();
-//            for (Object token : tokens) {
-//                if (token instanceof COSString) {
-//                    handleString((COSString)token, fragments, page);
-//                } else if (token instanceof COSArray) {
-//                    handleArray((COSArray)token, fragments, page);
-//                }
-//            }
-        }
+        StreamSupport.stream(doc.getPages().spliterator(), false).forEach(p -> {
+            try {
+                extractor.processPage(p);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
 
         return extractor.buildDocument();
-    }
-
-    private void handleString(COSString stringToken, List<ExtractedFragment> fragments, PDPage page) throws IOException {
-        fragments.add(ExtractedTextFragment.builder()
-                .text(new UnicodeTextDecoder(page).decode(stringToken))
-                .build());
-    }
-
-    private void handleArray(COSArray arrayToken, List<ExtractedFragment> fragments, PDPage page) throws IOException {
-        for (Object token : arrayToken.toList()) {
-            if (token instanceof COSString) {
-                handleString((COSString)token, fragments, page);
-            }
-        }
     }
 }
--- src/main/java/com/vuongideas/gamum/processor/core/PageExtractor.java
+++ src/main/java/com/vuongideas/gamum/processor/core/PageExtractor.java
@@ -3,18 +3,18 @@ package com.vuongideas.gamum.processor.core;
 import com.vuongideas.gamum.processor.model.ExtractedDocument;
 import com.vuongideas.gamum.processor.model.fragment.ExtractedFragment;
 import com.vuongideas.gamum.processor.model.fragment.ExtractedImageFragment;
+import com.vuongideas.gamum.processor.model.fragment.ExtractedTextFragment;
 import org.apache.pdfbox.contentstream.PDFStreamEngine;
 import org.apache.pdfbox.contentstream.operator.Operator;
 import org.apache.pdfbox.cos.COSBase;
 import org.apache.pdfbox.cos.COSName;
+import org.apache.pdfbox.cos.COSString;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.graphics.form.PDFormXObject;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,12 +28,27 @@ public class PageExtractor extends PDFStreamEngine {
     protected void processOperator(Operator operator, List<COSBase> operands) throws IOException {
         String operation = operator.getName();
         if ("Tj".equals(operation)) {
-            // TODO extract text
             System.out.println("show text");
 
+            COSString text = (COSString) operands.get(0);
+
+            if (currentFont != null) {
+                StringBuilder builder = new StringBuilder();
+
+                for (byte b : text.getBytes()) {
+                    builder.append(currentFont.toUnicode(b));
+                }
+                fragments.add(ExtractedTextFragment.builder().text(builder.toString()).build());
+            } else {
+                // attempt to just get the text
+                fragments.add(ExtractedTextFragment.builder().text(text.getString()).build());
+            }
+
         } else if ("Tf".equals(operation)) {
-            // TODO set font
             System.out.println("set font");
+
+            // get the name of font
+            currentFont = getResources().getFont((COSName) operands.get(0));
 
         } else if ("Do".equals(operation)) {
             COSName objectName = (COSName) operands.get(0);
